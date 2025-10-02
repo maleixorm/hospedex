2c8076a15f3a6e6fd351bc080cbcc141597b82a8
Marcos Aleixo <maknytz@gmail.com>
2025-10-02 18:29:29 -0300

    Implementando o programa de hospedagem

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using hospedex.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Marcos", sobrenome: "Marques");
+Pessoa p2 = new Pessoa(nome: "Leonardo", sobrenome: "Buta");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new Suite(tipoSuite: "Standard", capacidade: 2, valorDiaria: 120);
+
+Reserva reserva = new Reserva(diasReservados: 7);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor total da estadia: {reserva.CalcularValorDiaria().ToString("C")}");
